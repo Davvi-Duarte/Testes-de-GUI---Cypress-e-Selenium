917e14b746331971b84ae3c75ac2ca9abd89ac8b
Davvi Duarte Rodrigues <49027886+Davvi-Duarte@users.noreply.github.com>
2025-04-02 18:24:20 -0300

    automatização de testes
    criando novos testes

---
+++ cypress/e2e/automatizandoCompras.cy.js
@@ -0,0 +1,52 @@
+describe('Testes de compras', () => {
+    it('Compra com sucesso - standard_user', () => {
+        cy.login_sauce('standard_user', 'secret_sauce');
+        cy.get(':nth-child(1) > .pricebar > .btn_primary').click()
+        cy.get(':nth-child(2) > .pricebar > .btn_primary').click()
+        cy.get(':nth-child(3) > .pricebar > .btn_primary').click()
+        cy.get(':nth-child(4) > .pricebar > .btn_primary').click()
+        cy.get(':nth-child(5) > .pricebar > .btn_primary').click()
+        cy.get(':nth-child(6) > .pricebar > .btn_primary').click()
+        // Localizando o ícone pelo atributo 'data-icon' e clicando
+        cy.get('[data-icon="shopping-cart"]').click();
+        cy.get('.btn_action').click()
+        cy.get('[data-test="firstName"]').type('Lucas')
+        cy.get('[data-test="lastName"]').type('Almeida')
+        cy.get('[data-test="postalCode"]').type('12345678')
+        cy.get('.btn_primary.cart_button').click()
+        cy.get('.btn_action').click()
+        cy.get('.complete-header').contains('THANK YOU FOR YOUR ORDER')
+    })
+
+    describe('Testes de compras', () => {
+        it('Compra com responsividade', () => {
+            // Testando em uma tela menor (Mobile)
+            cy.viewport(375, 667);  // Simula um dispositivo mobile
+            cy.login_sauce('standard_user', 'secret_sauce');
+            cy.get(':nth-child(1) > .pricebar > .btn_primary').click();
+            cy.get('[data-icon="shopping-cart"]').click();
+            cy.get('.btn_action').click();
+            cy.get('[data-test="firstName"]').type('Lucas');
+            cy.get('[data-test="lastName"]').type('Almeida');
+            cy.get('[data-test="postalCode"]').type('12345678');
+            cy.get('.btn_primary.cart_button').click();
+            cy.get('.btn_action').click();
+            cy.get('.complete-header').contains('THANK YOU FOR YOUR ORDER');
+            
+            // Testando em uma tela maior (Desktop)
+            cy.viewport(1280, 800);  // Simula uma tela maior (Desktop)
+            cy.login_sauce('standard_user', 'secret_sauce');
+            cy.get(':nth-child(1) > .pricebar > .btn_primary').click();
+            cy.get('[data-icon="shopping-cart"]').click();
+            cy.get('.btn_action').click();
+            cy.get('[data-test="firstName"]').type('Lucas');
+            cy.get('[data-test="lastName"]').type('Almeida');
+            cy.get('[data-test="postalCode"]').type('12345678');
+            cy.get('.btn_primary.cart_button').click();
+            cy.get('.btn_action').click();
+            cy.get('.complete-header').contains('THANK YOU FOR YOUR ORDER');
+        })
+    })
+    
+
+})
